e51ce88b9b082314aef7277215b5953164d0dc80
zevero <zevero@users.noreply.github.com>
2014-12-17 03:14:35 +0100

    Create extendStorage.js

---
+++ extendStorage.js
@@ -0,0 +1,19 @@
+Storage.prototype.set = function(key, obj) {
+  var t = typeof obj;
+  if (t==='undefined' || obj===null ) this.removeItem(key);
+  this.setItem(key, (t==='object')?JSON.stringify(obj):obj);
+}
+Storage.prototype.get = function(key) {
+    var obj = this.getItem(key);
+    return obj && JSON.parse(obj);
+}
+Storage.prototype.has = this.hasOwnProperty;
+Storage.prototype.remove = this.removeItem;
+
+Storage.prototype.size = function(key){
+  var obj = this.getItem(key);
+  return obj?obj.length:0;
+}
+Storage.prototype.keys = function(){
+  Object.keys(this.valueOf());
+}
