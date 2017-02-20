366f88ac437f1662e978fe442e14e242486e4edd
Kai Falkowski <k.falkowski@reuter.de>
2017-02-20 10:20:08 +0100

    wrap storage extension into self executing function to avoid 'use strict' issues in global scope

--- extendStorage.js
+++ extendStorage.js
@@ -1,26 +1,30 @@
-/*globals Storage*/
-'use strict';
 /*
 Wonder how this works?
 Storage is the Prototype of both localStorage and sessionStorage.
 Got it?
 */
-Storage.prototype.set = function(key, obj) {
-  var t = typeof obj;
-  if (t==='undefined' || obj===null ) this.removeItem(key);
-  this.setItem(key, (t==='object')?JSON.stringify(obj):obj);
-};
-Storage.prototype.get = function(key) {
-    var obj = this.getItem(key);
-    try {
-      var j = JSON.parse(obj);
-      if (j && typeof j === "object") return j;
-    } catch (e) { }
-    return obj;
-};
-Storage.prototype.has = this.hasOwnProperty;
-Storage.prototype.remove = this.removeItem;
 
-Storage.prototype.keys = function(){
-  return Object.keys(this.valueOf());
-};
+(function() {
+  'use strict';
+
+  Storage.prototype.set = function(key, obj) {
+    var t = typeof obj;
+    if (t==='undefined' || obj===null ) window.removeItem(key);
+    window.setItem(key, (t==='object')?JSON.stringify(obj):obj);
+  };
+  Storage.prototype.get = function(key) {
+      var obj = window.getItem(key);
+      try {
+        var j = JSON.parse(obj);
+        if (j && typeof j === "object") return j;
+      } catch (e) { }
+      return obj;
+  };
+  Storage.prototype.has = window.hasOwnProperty;
+  Storage.prototype.remove = window.removeItem;
+
+  Storage.prototype.keys = function(){
+    return Object.keys(window.valueOf());
+  };
+
+})();
